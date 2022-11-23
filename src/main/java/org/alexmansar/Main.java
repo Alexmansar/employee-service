9f795de29a361de660af255b0b91dfebc910dd69
Oleksandr Mansarliyskyy <a.mansarliyskiy@gmail.com>
2022-11-23 17:58:31 +0200

    init commit

--- src/main/java/org/alexmansar/Main.java
+++ src/main/java/org/alexmansar/Main.java
@@ -1,4 +1,6 @@
-package org.alexmansar;public class Main {
+package org.alexmansar;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
